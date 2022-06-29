5144878da94e71b904bf319c5633a0d3663fcdee
jrubianom <jrubianom@unal.edu.co>
2022-06-28 21:45:26 -0500

    update

---
+++ local_config.mk
@@ -0,0 +1,16 @@
+#For source code instalations do
+MY_MFEM_INSTALL_DIR = /home/user_name/repos/MFEM/mfem/build
+HYPRE_INC = -I$(MY_MFEM_INSTALL_DIR)/../../hypre/src/hypre/include
+SUNDIALS_INC = -I$(MFEM_INSTALL_DIR)/../../sundials/install/include
+
+#For Spack instalations do
+#MY_MFEM_INSTALL_DIR = /opt/spack/opt/spack/linux-pop22-icelake/gcc-11.2.0/mfem-4.4.0-jttro3op37l25j7op5hhg4taglsotydj
+
+CONFIG_MK=$(MY_MFEM_INSTALL_DIR)/share/mfem/config.mk 
+GENERAL = $(MY_MFEM_INSTALL_DIR)/include/mfem/general
+
+SHARE_DIR = NULL
+PROCCESORS = 5
+
+include $(CONFIG_MK)
+
